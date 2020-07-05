7c3074f9400eb343c0f35e3215375583ed0d927e
Md Imran Ali <mdimranali94@gmail.com>
2020-07-05 16:23:25 +0530

    first commit

---
+++ TestDemo/RViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RViewController.swift
+//  TestDemo
+//
+//  Created by Kann on 11/10/19.
+//  Copyright © 2019 Kann. All rights reserved.
+//
+
+import UIKit
+
+class RViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
